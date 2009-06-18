09a8d62ae96093869ac2e125d898b43be744afff
Windix Feng <windix@gmail.com>
2009-06-18 10:50:49 +1000

    Chap2

--- money_test.rb
+++ money_test.rb
@@ -4,7 +4,11 @@ require 'money'
 class MoneyTest < Test::Unit::TestCase
   def test_multiplication
     five = Dollar.new(5)
-    five.times(2)
-    assert_equal(10, five.amount)
+    
+    product = five.times(2)
+    assert_equal(10, product.amount)
+    
+    product = five.times(3)
+    assert_equal(15, product.amount)
   end
 end
