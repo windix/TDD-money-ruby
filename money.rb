ae0224a4e1f3079f63b876f97036a399f5d4bc46
Windix Feng <windix@gmail.com>
2009-06-18 12:45:08 +1000

    Chap5

--- money.rb
+++ money.rb
@@ -14,3 +14,20 @@ class Dollar
     @amount == dollar.amount
   end
 end
+
+class Franc
+  # Chap3: In ruby: this cannot be set to private
+  attr_accessor :amount
+
+  def initialize(amount)
+    @amount = amount
+  end
+
+  def times(multiplier)
+    Franc.new(@amount * multiplier)
+  end
+
+  def ==(franc)
+    @amount == franc.amount
+  end
+end
--- money_test.rb
+++ money_test.rb
@@ -2,12 +2,18 @@ require 'test/unit'
 require 'money'
 
 class MoneyTest < Test::Unit::TestCase
-  def test_multiplication
+  def test_dollar_multiplication
     five = Dollar.new(5)
     assert_equal(Dollar.new(10), five.times(2))
     assert_equal(Dollar.new(15), five.times(3))
   end
-  
+ 
+  def test_franc_multiplication
+    five = Franc.new(5)
+    assert_equal(Franc.new(10), five.times(2))
+    assert_equal(Franc.new(15), five.times(3))
+  end
+
   def test_equality
     assert(Dollar.new(5) == Dollar.new(5))
     assert_equal(false, Dollar.new(5) == Dollar.new(6))
