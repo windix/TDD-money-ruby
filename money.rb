e63344456b468412b4c0bcbe02add42b4ceadb12
Windix Feng <windix@gmail.com>
2009-06-18 13:56:21 +1000

    Chap9

--- money.rb
+++ money.rb
@@ -1,9 +1,11 @@
 class Money
   # Chap3: In ruby: this cannot be set to private
   attr_accessor :amount
+  attr_reader :currency
   
-  def initialize(amount)
+  def initialize(amount, currency)
     @amount = amount
+    @currency = currency
   end
 
   def ==(money)
@@ -12,25 +14,24 @@ class Money
 
   def self.dollar(amount)
     # Chap 8: How to cast from Dollar to parent Money?
-    Dollar.new(amount)
+    Dollar.new(amount, "USD")
   end
   
   def self.franc(amount)
     # Chap 8: How to cast from Dollar to parent Money?
-    Franc.new(amount)
+    Franc.new(amount, "CHF")
   end
 end
 
 class Dollar < Money
   def times(multiplier)
-    Dollar.new(@amount * multiplier)
+    Money.dollar(@amount * multiplier)
   end
-
 end
 
 class Franc < Money
   def times(multiplier)
-    Franc.new(@amount * multiplier)
+    Money.franc(@amount * multiplier)
   end
 end
 
--- money_test.rb
+++ money_test.rb
@@ -23,4 +23,9 @@ class MoneyTest < Test::Unit::TestCase
     
     assert_equal(false, Money.dollar(5) == Money.franc(5))
   end
+
+  def test_currency
+    assert_equal("USD", Money.dollar(1).currency)
+    assert_equal("CHF", Money.franc(1).currency)
+  end
 end
