2cda8bf05b5a3ce35347f6646471a6f4ed303f96
Windix Feng <windix@gmail.com>
2009-06-18 10:59:54 +1000

    Chap3

--- money_test.rb
+++ money_test.rb
@@ -11,4 +11,9 @@ class MoneyTest < Test::Unit::TestCase
     product = five.times(3)
     assert_equal(15, product.amount)
   end
+  
+  def test_equality
+    assert(Dollar.new(5) == Dollar.new(5))
+    assert_equal(false, Dollar.new(5) == Dollar.new(6))
+  end
 end
